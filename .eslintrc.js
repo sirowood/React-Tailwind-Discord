b4a2a0bea51f9e723be3dc43b7131c339a17000c
Xuefeng Wu <sirowood@gmail.com>
2022-10-18 11:43:52 +0300

    prettier and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    compress: true,
-    static: path.resolve(__dirname, 'dist'),
-    historyApiFallback: true,
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
-  ],
+	entry: './index.jsx',
+	mode: 'production',
+	output: {
+		path: path.resolve(__dirname, '/dist'),
+		filename: 'main.js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	devServer: {
+		port: 3000,
+		open: true,
+		compress: true,
+		static: path.resolve(__dirname, 'dist'),
+		historyApiFallback: true,
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
