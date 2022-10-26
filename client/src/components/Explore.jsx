68f3125e0ed46bac5637517725871fb26e3c74bc
Xuefeng Wu <sirowood@gmail.com>
2022-10-26 14:29:45 +0300

    refactor explore

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -1,7 +1,48 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import {
  Explore as ExploreIcon, Gaming, Music, Education, Science, Entertainment, StudentHub,
 } from './icons';
+
+const exploreData = [
+	{
+		name: 'Home',
+		icon: <ExploreIcon className="w-6 h-6" />,
+	},
+	{
+		name: 'Gaming',
+		icon: <Gaming className="w-6 h-6" />,
+	},
+	{
+		name: 'Music',
+		icon: <Music className="w-6 h-6" />,
+	},
+	{
+		name: 'Education',
+		icon: <Education className="w-6 h-6" />,
+	},
+	{
+		name: 'Science & tech',
+		icon: <Science className="w-6 h-6" />,
+	},
+	{
+		name: 'Entertainment',
+		icon: <Entertainment className="w-6 h-6" />,
+	},
+	{
+		name: 'Student Hubs',
+		icon: <StudentHub className="w-6 h-6" />,
+	},
+];
+
+function ExploreRow({ rowName, children }) {
+	return (
+		<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
+			<div className="flex items-center justify-center w-8 h-8 mr-3">{children}</div>
+			<div>{rowName}</div>
+		</div>
+	);
+}
 
 export default function Explore() {
 	return (
@@ -12,40 +53,11 @@ export default function Explore() {
 						Discover
 					</div>
 					<div className="flex flex-col gap-y-[2px] py-px ml-2 h-11">
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><ExploreIcon className="w-6 h-6" /></div>
-							<div>Home</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><Gaming className="w-6 h-6" /></div>
-							<div>Gaming</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><Music className="w-6 h-6" /></div>
-							<div>Music</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><Education className="w-6 h-6" /></div>
-							<div>Education</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><Science className="w-6 h-6" /></div>
-							<div>Science & tech</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><Entertainment className="w-6 h-6" /></div>
-							<div>Entertainment</div>
-						</div>
-
-						<div className="flex h-full rounded px-2 py-[5px] items-center text-gray-200 text-base leading-5 font-[500] hover:cursor-pointer hover:bg-gray-650 hover:text-gray-100">
-							<div className="flex items-center justify-center w-8 h-8 mr-3"><StudentHub className="w-6 h-6" /></div>
-							<div>Student Hubs</div>
-						</div>
+						{exploreData.map((item) => (
+							<ExploreRow key={item.name} rowName={item.name}>
+								{item.icon}
+							</ExploreRow>
+						))}
 					</div>
 				</div>
 				<div className="absolute bottom-0 bg-red-500 w-60 h-[52px] px-2">
