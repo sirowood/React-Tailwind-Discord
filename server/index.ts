bfc700970cb8eedb4c493a63b9dfcc0659220ed4
Xuefeng Wu <sirowood@gmail.com>
2022-11-19 12:16:33 +0200

    #deploy turn server to ts

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import cors from 'cors';
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors());
+app.use(express.static('dist'));
+
+app.use('/health', (_req, res) => {
+  res.send('ok');
+});
+
+app.use('*', (_req, res) => {
+  res.redirect(301, '/');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
